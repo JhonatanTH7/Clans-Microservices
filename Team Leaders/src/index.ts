5c5a933678f702412c0b665b3a66703885cc9f08
JhonatanTH7 <Jhonatantorohurtado93@gmail.com>
2024-07-24 13:04:55 -0500

    update

--- Team Leaders/src/index.ts
+++ Team Leaders/src/index.ts
@@ -1,4 +1,8 @@
 import express, { Request, Response } from "express";
+import { AppDataSource } from "./config/connection-db";
+import { error } from "console";
+import teamLeaderRouter from "./router/team-leader.route";
+import { client } from "./config/eureka-client";
 
 const app = express();
 app.use(express.json());
@@ -7,6 +11,19 @@ app.get("/", (req: Request, res: Response) => {
   res.json({ message: "Hello World!" });
 });
 
-app.listen(7000, () => {
-  console.log("Server is running on port 7000");
-});
+app.use("api/v1/team-leader", teamLeaderRouter);
+
+AppDataSource.initialize()
+  .then(() => {
+    console.log("Database connected");
+
+    app.listen(7000, () => {
+      console.log("Server is running on port 7000");
+      client.start((error) => {
+        console.log(error || "Eureka Client started");
+      });
+    });
+  })
+  .catch((error) => {
+    console.log("Error connecting to the database ", error);
+  });
